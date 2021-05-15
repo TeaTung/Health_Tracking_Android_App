908516265cacd4cf88a42510bdc061a46c5b894e
Thắng Nguyễn Võ Đức <19522213@ms.uit.edu.vn>
2021-05-15 11:23:43 +0700

    Step counter v2

--- app/src/main/java/com/example/healthtracking/MainActivity.java
+++ app/src/main/java/com/example/healthtracking/MainActivity.java
@@ -81,9 +81,6 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences sharedPreferences = getSharedPreferences("sharedPrefs",MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
 
-        Calendar today = Calendar.getInstance();
-        int currentDay = today.get(Calendar.DAY_OF_MONTH);
-        editor.putInt("TODAY",currentDay);
         editor.putBoolean("WAS_LOGIN",true);
         editor.putBoolean("WAS_INFORMATION",false);
         editor.apply();
