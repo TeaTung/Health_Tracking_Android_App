457f9fe5c8fb86c257df664252f61e027ae338c5
Thắng Nguyễn Võ Đức <19522213@ms.uit.edu.vn>
2021-06-01 20:49:55 +0700

    tracking step

--- app/src/main/java/com/example/healthtracking/MapRunActivity.java
+++ app/src/main/java/com/example/healthtracking/MapRunActivity.java
@@ -2,13 +2,17 @@ package com.example.healthtracking;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -19,15 +23,25 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 import org.jetbrains.annotations.NotNull;
 
-public class MapRunActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapRunActivity extends AppCompatActivity implements OnMapReadyCallback {
     SupportMapFragment supportMapFragment;
     FusedLocationProviderClient client;
     View decorateView;
+    ImageView imgStart;
+    Button btnStop;
+    ConstraintLayout lytNotification;
+    MarkerOptions startMarkerOptions;
+    List<LatLng> lngList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +52,7 @@ public class MapRunActivity extends AppCompatActivity {
         client = LocationServices.getFusedLocationProviderClient(this);
         supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.google_map);
+        supportMapFragment.getMapAsync(this);
         //Decorate view
         decorateView = getWindow().getDecorView();
         decorateView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
@@ -49,8 +64,17 @@ public class MapRunActivity extends AppCompatActivity {
             }
         });
 
+        //Assign implement
+        imgStart = (ImageView) findViewById(R.id.imgStart);
+        btnStop = (Button) findViewById(R.id.btnStop);
+        lytNotification = (ConstraintLayout) findViewById(R.id.notification_background);
+        lngList = new ArrayList<LatLng>();
+
+        //Call function
         checkAccessLocationPermission();
         getCurrentLocation();
+        callButtonStop();
+        callImageStart();
 
     }
 
@@ -65,13 +89,7 @@ public class MapRunActivity extends AppCompatActivity {
     }
     private void getCurrentLocation() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+
             return;
         }
         Task<Location> task = client.getLastLocation();
@@ -86,13 +104,23 @@ public class MapRunActivity extends AppCompatActivity {
                             //Initialize lat Lng
                             LatLng latLng = new LatLng(location.getLatitude(),
                                     location.getLongitude());
-
                             //Create maker options
-                            MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Bạn đang ở đây");
+                            startMarkerOptions = new MarkerOptions().position(latLng).title("Bạn đang ở đây");
 
                             //Zoom map
-                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
-                            googleMap.addMarker(markerOptions);
+                            googleMap.addMarker(startMarkerOptions);
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+
+                            PolylineOptions line = new PolylineOptions().width(15).color(Color.BLUE);
+                            line.add(startMarkerOptions.getPosition());
+
+                            googleMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+                                @Override
+                                public void onMyLocationChange(@NonNull @NotNull Location location) {
+                                    line.add(new LatLng(location.getLatitude(),location.getLongitude()));
+                                    Polyline polyline = googleMap.addPolyline(line);
+                                }
+                            });
                         }
                     });
                 }
@@ -107,7 +135,6 @@ public class MapRunActivity extends AppCompatActivity {
             }
         }
     }
-
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
@@ -115,7 +142,6 @@ public class MapRunActivity extends AppCompatActivity {
             decorateView.setSystemUiVisibility(hideSystemBar());
         }
     }
-
     private int hideSystemBar() {
         return View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
@@ -123,5 +149,32 @@ public class MapRunActivity extends AppCompatActivity {
                 |View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 |View.SYSTEM_UI_FLAG_FULLSCREEN
                 |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+    }
+    public void callImageStart(){
+        imgStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnStop.setText("STOP");
+                imgStart.setVisibility(View.INVISIBLE);
+                lytNotification.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+    public void callButtonStop(){
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (btnStop.getText().equals("STOP")) {
+                    btnStop.setText("OK");
+                } else if (btnStop.getText().equals("OK")){
+                    lytNotification.setVisibility(View.INVISIBLE);
+                    imgStart.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+    @Override
+    public void onMapReady(@NonNull @NotNull GoogleMap googleMap) {
+
     }
 }
