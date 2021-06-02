809a29432ed8bbb9e7598164b56000786b9673a4
Thắng Nguyễn Võ Đức <19522213@ms.uit.edu.vn>
2021-06-02 18:28:34 +0700

    tracking step

--- app/src/main/java/com/example/healthtracking/MapRunActivity.java
+++ app/src/main/java/com/example/healthtracking/MapRunActivity.java
@@ -2,15 +2,28 @@ package com.example.healthtracking;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,15 +32,32 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 import org.jetbrains.annotations.NotNull;
 
-public class MapRunActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapRunActivity extends AppCompatActivity implements OnMapReadyCallback, SensorEventListener {
     SupportMapFragment supportMapFragment;
     FusedLocationProviderClient client;
     View decorateView;
+    ImageView imgStart;
+    Button btnStop;
+    ConstraintLayout lytNotification;
+    FusedLocationProviderClient fusedLocationClient;
+    SensorManager sensorManager;
+    TextView tvStep, tvTimeRecord, tvDistance, tvSpeed, tvCalo;
+    ProgressBar pgbAward;
+    int step;
+    double distance;
+    double calo;
+    double speed;
+    int time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,21 +68,40 @@ public class MapRunActivity extends AppCompatActivity {
         client = LocationServices.getFusedLocationProviderClient(this);
         supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.google_map);
+        supportMapFragment.getMapAsync(this);
         //Decorate view
         decorateView = getWindow().getDecorView();
         decorateView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
             @Override
             public void onSystemUiVisibilityChange(int visibility) {
-                if (visibility == 0){
+                if (visibility == 0) {
                     decorateView.setSystemUiVisibility(hideSystemBar());
                 }
             }
         });
 
+        //Assign implement
+        imgStart = (ImageView) findViewById(R.id.imgStart);
+        btnStop = (Button) findViewById(R.id.btnStop);
+        lytNotification = (ConstraintLayout) findViewById(R.id.notification_background);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        tvStep = (TextView)findViewById(R.id.tvStep);
+        tvCalo = (TextView)findViewById(R.id.tvCalo);
+        tvDistance = (TextView)findViewById(R.id.tvDistance);
+        tvSpeed = (TextView)findViewById(R.id.tvSpeed);
+        tvTimeRecord = (TextView)findViewById(R.id.tvTimeRecord);
+        pgbAward = (ProgressBar)findViewById(R.id.pgbAward);
+        pgbAward.setMax(100);
+
+        //Call function
         checkAccessLocationPermission();
         getCurrentLocation();
+        callButtonStop();
+        callImageStart();
 
     }
+
 
     public void checkAccessLocationPermission() {
         if (ActivityCompat.checkSelfPermission(MapRunActivity.this,
@@ -63,41 +112,80 @@ public class MapRunActivity extends AppCompatActivity {
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
         }
     }
+
     private void getCurrentLocation() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+
             return;
         }
-        Task<Location> task = client.getLastLocation();
-        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+//        Task<Location> task = client.getLastLocation();
+//        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+//            @Override
+//            public void onSuccess(Location location) {
+//                if (location != null){
+//                    //sync map
+//                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+//                        @Override
+//                        public void onMapReady(@NonNull @NotNull GoogleMap googleMap) {
+//                            //Initialize lat Lng
+//                            LatLng latLng = new LatLng(location.getLatitude(),
+//                                    location.getLongitude());
+//                            //Create maker options
+//                            MarkerOptions startMarkerOptions = new MarkerOptions().position(latLng).title("Bạn đang ở đây");
+//
+//                            //Zoom map
+//                            googleMap.addMarker(startMarkerOptions);
+//                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+//
+//                            PolylineOptions line = new PolylineOptions().width(15).color(Color.BLUE);
+//                            line.add(startMarkerOptions.getPosition());
+//
+//                            googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+//                                @Override
+//                                public void onMapClick(@NonNull @NotNull LatLng latLng) {
+//                                    line.add(latLng);
+//                                    Polyline polyline = googleMap.addPolyline(line);
+//                                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+//                                }
+//                            });
+//                        }
+//                    });
+//                }
+//            }
+//        });
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        Task<Location> locationTask = fusedLocationClient.getLastLocation();
+        locationTask.addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
-                if (location != null){
+                if(location != null){
                     //sync map
                     supportMapFragment.getMapAsync(new OnMapReadyCallback() {
                         @Override
                         public void onMapReady(@NonNull @NotNull GoogleMap googleMap) {
-                            //Initialize lat Lng
-                            LatLng latLng = new LatLng(location.getLatitude(),
+                            //Create marker
+                            LatLng startLocation = new LatLng(location.getLatitude(),
                                     location.getLongitude());
+                            MarkerOptions startMaker = new MarkerOptions().position(startLocation)
+                                    .title("Bạn đang ở đây");
 
-                            //Create maker options
-                            MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Bạn đang ở đây");
+                            //Zoom map and add marker
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(startLocation,15));
+                            googleMap.addMarker(startMaker);
 
-                            //Zoom map
-                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
-                            googleMap.addMarker(markerOptions);
+                            PolylineOptions line = new PolylineOptions().width(15).color(Color.BLUE);
+                            line.add(startLocation);
+                            drawLastLocation(googleMap,line);
                         }
                     });
                 }
             }
         });
+
     }
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
@@ -107,7 +195,6 @@ public class MapRunActivity extends AppCompatActivity {
             }
         }
     }
-
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
@@ -115,7 +202,6 @@ public class MapRunActivity extends AppCompatActivity {
             decorateView.setSystemUiVisibility(hideSystemBar());
         }
     }
-
     private int hideSystemBar() {
         return View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
@@ -123,5 +209,67 @@ public class MapRunActivity extends AppCompatActivity {
                 |View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 |View.SYSTEM_UI_FLAG_FULLSCREEN
                 |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+    }
+    public void callImageStart(){
+        imgStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnStop.setText("STOP");
+                imgStart.setVisibility(View.INVISIBLE);
+                lytNotification.setVisibility(View.VISIBLE);
+
+                step = 0;
+            }
+        });
+    }
+    public void callButtonStop(){
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (btnStop.getText().equals("STOP")) {
+                    btnStop.setText("OK");
+                } else if (btnStop.getText().equals("OK")){
+                    lytNotification.setVisibility(View.INVISIBLE);
+                    imgStart.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+    @Override
+    public void onMapReady(@NonNull @NotNull GoogleMap googleMap) {
+
+    }
+    public void drawLastLocation(GoogleMap googleMap, PolylineOptions line) {
+        LocationCallback locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull @NotNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                LatLng latLng = new LatLng(locationResult.getLastLocation().getLatitude()
+                        , locationResult.getLastLocation().getLongitude());
+
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+                line.add(latLng);
+            }
+        };
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+//        step++;
+//        distance = Math.round(step* 0.7*100)/100;
+//        calo = distance * 0.0625;
+//        speed = distance / time;
+//
+//        tvStep.setText("" + step);
+//        tvDistance.setText("" + distance);
+//        tvCalo.setText("" + calo);
+//        tvSpeed.setText("" + speed);
+//        pgbAward.setProgress(step);
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+    public void runTimer(){
+
     }
 }
